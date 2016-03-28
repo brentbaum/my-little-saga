5c027673930bf1d352678e02b111fa2fbe9e7ff1
Brent Baumgartner <brent@brentbaum.com>
2016-03-28 10:17:38 -0400

    Serve map creator at /creator

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 var process = require('process');
 var http = require('http').Server(app);
-app.use(express.static('./'));
+
+app.use('/', express.static(__dirname + '/'));
+app.use('/creator', express.static(__dirname + '/map-creator/app'));
+
 var server = app.listen(process.env.PORT || 5000);
 
 console.log("Server started.");;
