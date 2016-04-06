71b4d2bd0db36e5fb0658973c5a557532d49e241
Brent Baumgartner <brent@brentbaum.com>
2016-04-05 20:14:49 -0400

    Shift viewport

--- map-creator/app/map-view/map-view.js
+++ map-creator/app/map-view/map-view.js
@@ -10,153 +10,185 @@ angular.module('myApp.mapView', ['ngRoute'])
 }])
 
 .controller('MapViewCtrl', ["$scope", function($scope) {
-    $scope.pencilTip = "tree";
-    $scope.layer = "background";
-    $scope.message = "Here is a map. It is blank";
-    $scope.size = {
-        x: 30,
-        y: 20
-    };
-
-    var range = function(start, max) {
-        var m = [];
-        for(var x = start; x < max; x++) {
-            m.push(x);
-        }
-        return m;
-    };
-
-    $scope.colRange = function() {
-        return range(0, $scope.size.x);
-    };
-
-    $scope.rowRange = function() {
-        return range(0, $scope.size.y);
-    };
-
-    $scope.backType = function(row, col) {
-        return $scope.map.background[row][col];
-    };
-
-    $scope.foreType = function(row, col) {
-        return $scope.map.foreground[row][col];
-    };
-
-    function createBlankMap(rows, cols) {
-        var map = {
-            background: [],
-            foreground: []
+        $scope.pencilTip = "tree";
+        $scope.layer = "background";
+        $scope.message = "Here is a map. It is blank";
+        $scope.size = {
+            x: 30,
+            y: 20
         };
-        for (var x = 0; x < cols; x++) {
-            var bRow = [];
-            var fRow = [];
-            for (var y = 0; y < rows; y++) {
-                bRow.push("grass");
-                fRow.push("");
+
+        $scope.view = {
+            x: 0,
+            y: 0
+        };
+        $scope.viewSize = {
+            x: 20,
+            y: 20
+        };
+
+        var range = function(start, max) {
+            var m = [];
+            for (var x = start; x < max; x++) {
+                m.push(x);
             }
-            map.background.push(bRow);
-            map.foreground.push(fRow);
-        }
-        return map;
-    };
+            return m;
+        };
 
-    var isMouseDown = false;
+        $scope.colRange = function() {
+            return range($scope.view.x, $scope.view.x + $scope.viewSize.x);
+        };
 
-    $scope.mouseDown = function() {
-        console.log("down");
-        isMouseDown = true;
-    };
+        $scope.rowRange = function() {
+            return range($scope.view.y, $scope.view.y + $scope.viewSize.y);
+        };
 
-    $scope.mouseUp = function() {
-        console.log("up");
-        isMouseDown = false;
-    };
+        $scope.backType = function(row, col) {
+            return $scope.map.background[row][col];
+        };
 
-    $scope.setTile = function(r, c) {
-        if (isMouseDown) {
+        $scope.foreType = function(row, col) {
+            return $scope.map.foreground[row][col];
+        };
+
+        $scope.shiftView = function(dir) {
+            if (dir === 'up') {
+                $scope.view.y -= 10;
+                if($scope.view.y < 0)
+                    $scope.view.y = 0;
+            }
+            if (dir === 'down') {
+                $scope.view.y += 20;
+                if($scope.view.y + $scope.viewSize.y > $scope.size.y)
+                    $scope.view.y = $scope.size.y - $scope.viewSize.y;
+            }
+            if (dir === 'left') {
+                $scope.view.x -= 20;
+                if($scope.view.x < 0)
+                    $scope.view.x = 0;
+            }
+            if (dir === 'right') {
+                $scope.view.x += 20;
+                if($scope.view.x + $scope.viewSize.x > $scope.size.x)
+                    $scope.view.x = $scope.size.x - $scope.viewSize.x;
+            }
+        };
+
+        function createBlankMap(rows, cols) {
+            var map = {
+                background: [],
+                foreground: []
+            };
+            for (var x = 0; x < cols; x++) {
+                var bRow = [];
+                var fRow = [];
+                for (var y = 0; y < rows; y++) {
+                    bRow.push("grass");
+                    fRow.push("");
+                }
+                map.background.push(bRow);
+                map.foreground.push(fRow);
+            }
+            return map;
+        };
+
+        var isMouseDown = false;
+
+        $scope.mouseDown = function() {
+            console.log("down");
+            isMouseDown = true;
+        };
+
+        $scope.mouseUp = function() {
+            console.log("up");
+            isMouseDown = false;
+        };
+
+        $scope.setTile = function(r, c) {
+            if (isMouseDown) {
+                $scope.map[$scope.layer][r][c] = $scope.pencilTip;
+            }
+        };
+
+        $scope.clickTile = function(r, c) {
             $scope.map[$scope.layer][r][c] = $scope.pencilTip;
-        }
-    };
+        };
 
-    $scope.clickTile = function(r, c) {
-        $scope.map[$scope.layer][r][c] = $scope.pencilTip;
-    };
+        $scope.clearGrid = function(override) {
+            if (!override) {
+                var result = confirm("You're about to clear the map! Did you mean to do this?");
+                if (!result)
+                    return;
+            }
+            $scope.map = createBlankMap($scope.size.x, $scope.size.y);
+        };
+        $scope.clearGrid(true);
 
-    $scope.clearGrid = function(override) {
-        if (!override) {
-            var result = confirm("You're about to clear the map! Did you mean to do this?");
-            if (!result)
-                return;
-        }
-        $scope.map = createBlankMap($scope.size.x, $scope.size.y);
-    };
-    $scope.clearGrid(true);
+        $scope.setTileType = function(type) {
+            $scope.pencilTip = type;
+        };
 
-    $scope.setTileType = function(type) {
-        $scope.pencilTip = type;
-    };
+        $scope.setLayer = function(type) {
+            $scope.layer = type;
+        };
 
-    $scope.setLayer = function(type) {
-        $scope.layer = type;
-    };
+        $scope.downloadCurrentMap = function() {
+            var str = JSON.stringify($scope.map, undefined, 2);
+            var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(str);
+            var dlAnchorElem = document.getElementById('downloadAnchorElem');
+            dlAnchorElem.setAttribute("href", dataStr);
+            dlAnchorElem.setAttribute("download", "map.json");
+            dlAnchorElem.click();
+        };
 
-    $scope.downloadCurrentMap = function() {
-        var str = JSON.stringify($scope.map, undefined, 2);
-        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(str);
-        var dlAnchorElem = document.getElementById('downloadAnchorElem');
-        dlAnchorElem.setAttribute("href", dataStr);
-        dlAnchorElem.setAttribute("download", "map.json");
-        dlAnchorElem.click();
-    };
+        $scope.mapJson = "";
 
-    $scope.mapJson = "";
+        $scope.$watch("mapJson", function(val) {
+            if (!!val) {
+                $scope.map = JSON.parse(val);
+                $scope.mapJSON = "";
+            }
+        });
 
-    $scope.$watch("mapJson", function(val) {
-        if(!!val) {
-            $scope.map = JSON.parse(val);
-            $scope.mapJSON = "";
-        }
+        $scope.tileTypes = ["grass", "tree", "rock", "water"];
+    }])
+    .directive('appFilereader', function($q) {
+        var slice = Array.prototype.slice;
+
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: function(scope, element, attrs, ngModel) {
+                    if (!ngModel) return;
+
+                    ngModel.$render = function() {};
+
+                    element.bind('change', function(e) {
+                        var element = e.target;
+
+                        $q.all(slice.call(element.files, 0).map(readFile))
+                            .then(function(values) {
+                                if (element.multiple) ngModel.$setViewValue(values);
+                                else ngModel.$setViewValue(values.length ? values[0] : null);
+                            });
+
+                        function readFile(file) {
+                            var deferred = $q.defer();
+
+                            var reader = new FileReader();
+                            reader.onload = function(e) {
+                                deferred.resolve(e.target.result);
+                            };
+                            reader.onerror = function(e) {
+                                deferred.reject(e);
+                            };
+                            reader.readAsText(file);
+
+                            return deferred.promise;
+                        }
+
+                    }); //change
+
+                } //link
+        }; //return
     });
-
-    $scope.tileTypes = ["grass", "tree", "rock", "water"];
-}])
-.directive('appFilereader', function($q) {
-    var slice = Array.prototype.slice;
-
-    return {
-        restrict: 'A',
-        require: '?ngModel',
-        link: function(scope, element, attrs, ngModel) {
-                if (!ngModel) return;
-
-                ngModel.$render = function() {};
-
-                element.bind('change', function(e) {
-                    var element = e.target;
-
-                    $q.all(slice.call(element.files, 0).map(readFile))
-                        .then(function(values) {
-                            if (element.multiple) ngModel.$setViewValue(values);
-                            else ngModel.$setViewValue(values.length ? values[0] : null);
-                        });
-
-                    function readFile(file) {
-                        var deferred = $q.defer();
-
-                        var reader = new FileReader();
-                        reader.onload = function(e) {
-                            deferred.resolve(e.target.result);
-                        };
-                        reader.onerror = function(e) {
-                            deferred.reject(e);
-                        };
-                        reader.readAsText(file);
-
-                        return deferred.promise;
-                    }
-
-                }); //change
-
-            } //link
-    }; //return
-});
