654485a29b7479462a0a1f0a856956483e1f08a8
Brent Baumgartner <brent@brentbaum.com>
2016-04-17 15:09:09 -0400

    Fix collisions and camera bug.

--- Prototype.js
+++ Prototype.js
@@ -1,261 +1,329 @@
 "use strict";
 
-var dev_inventory = [{id: "rock", name: "Rock", description: "For smashing", count: 3},
-		     {id: "tree", name: "Tree", description: "For building", count: 3}];
-var dev_game_state = {actionsUnlocked: ["berserk", "law", "melee", "magic"],
-		      inBattle: false,
-		      inventory: []
-		     };
-var game_size = {x: 1000, y: 600};
+var dev_inventory = [{
+    id: "rock",
+    name: "Rock",
+    description: "For smashing",
+    count: 3
+}, {
+    id: "tree",
+    name: "Tree",
+    description: "For building",
+    count: 3
+}];
+var dev_game_state = {
+    actionsUnlocked: ["berserk", "law", "melee", "magic"],
+    inBattle: false,
+    inventory: []
+};
+var game_size = {
+    x: 1000,
+    y: 600
+};
 
 var GameObjects = {
-    hero: {name: "hero",
-	   filename: "super-mario-sprite.png"},
-    rock: {name: "rock",
-	   filename: "fg/rock.png"},
-    grass: {name: "grass",
-	    filename: "bg/grass.png"},
-    water: {name: "water",
-	    filename: "bg/water.png"},
-    sand: {name: "sand",
-	   filename: "bg/sand.png"},
-    bear: {name: "bear",
-	   filename: "fg/bear.png"}
+    hero: {
+        name: "hero",
+        filename: "super-mario-sprite.png"
+    },
+    rock: {
+        name: "rock",
+        filename: "fg/rock.png"
+    },
+    grass: {
+        name: "grass",
+        filename: "bg/grass.png"
+    },
+    water: {
+        name: "water",
+        filename: "bg/water.png"
+    },
+    sand: {
+        name: "sand",
+        filename: "bg/sand.png"
+    },
+    bear: {
+        name: "bear",
+        filename: "fg/bear.png"
+    }
 };
 
 class Saga extends Game {
     constructor(canvas) {
-	var size = game_size;
-	super("My Little Saga <3", size.x, size.y, canvas);
-	this.size = size;
-	this.centerPoint = {
-	    x: this.size.x / 2,
-	    y: this.size.y / 2
-	};
-	this.tileSize = 64;
-	this.tileCount = {
-	    x: 25,
-	    y: 20
-	};
-	this.mapSize = {
-	    x: this.tileCount.x * this.tileSize,
-	    y: this.tileCount.y * this.tileSize
-	};
-	this.root = new DisplayObjectContainer("Root");
-	this.inBattle = false;
-	this.tweener = new TweenJuggler();
-	this.toastManager = new ToastManager();
-	this.dispatcher = new EventDispatcher();
-	this.questManager = new QuestManager();
-	this.combatManager = new CombatManager();
-	this.inventoryManager = new InventoryManager();
-	this.dispatcher.addEventListener(this.questManager, "collision");
-	this.dispatcher.addEventListener(this.questManager, "proximity-collision");
-	this.actionManager = new ActionManager();
-	this.addActions();
-	this.sound = new SoundManager();
+        var size = game_size;
+        super("My Little Saga <3", size.x, size.y, canvas);
+        this.size = size;
+        this.centerPoint = {
+            x: this.size.x / 2,
+            y: this.size.y / 2
+        };
+        this.tileSize = 64;
+        this.tileCount = {
+            x: 25,
+            y: 20
+        };
+        this.mapSize = {
+            x: this.tileCount.x * this.tileSize,
+            y: this.tileCount.y * this.tileSize
+        };
+        this.root = new DisplayObjectContainer("Root");
+        this.inBattle = false;
+        this.tweener = new TweenJuggler();
+        this.toastManager = new ToastManager();
+        this.dispatcher = new EventDispatcher();
+        this.questManager = new QuestManager();
+        this.combatManager = new CombatManager();
+        this.inventoryManager = new InventoryManager();
+        this.dispatcher.addEventListener(this.questManager, "collision");
+        this.dispatcher.addEventListener(this.questManager, "proximity-collision");
+        this.actionManager = new ActionManager();
+        this.addActions();
+        this.sound = new SoundManager();
 
-	this.setupHero();
+        this.setupHero();
 
-	this.gameState = dev_game_state;
-	// this.gameState = {actionsUnlocked: [], reputation: 0};
+        this.gameState = dev_game_state;
+        // this.gameState = {actionsUnlocked: [], reputation: 0};
 
-	//var mapGenerator = new MapGenerator();
-	//var map = mapGenerator.generate(this.tileCount);
-	var mapReader = new MapReader();
-	var tileReader = new TileMappingReader();
-	var saga = this;
-	this.tileMapping = {};
-	var t = this;
-	tileReader.get(function(mapping) {
-	    mapping.list.forEach(function(tile) {
-		t.tileMapping[tile.key] = tile.img;
-	    });
-	    mapReader.get('berserkerforest', function(map) {
-		saga.setupMap(saga.root, map);
-	    });
-	});
+        //var mapGenerator = new MapGenerator();
+        //var map = mapGenerator.generate(this.tileCount);
+        var mapReader = new MapReader();
+        var tileReader = new TileMappingReader();
+        var saga = this;
+        this.tileMapping = {};
+        var t = this;
+        tileReader.get(function(mapping) {
+            mapping.list.forEach(function(tile) {
+                t.tileMapping[tile.key] = tile.img;
+            });
+            mapReader.get('berserkerforest', function(map) {
+                saga.setupMap(saga.root, map);
+            });
+        });
     }
 
     setupHero(root) {
-	var heroAnimations = {
-	    "stop": {
-		start: 0,
-		end: 0
-	    },
-	    "run": {
-		start: 1,
-		end: 3
-	    },
-	    "hit": {
-		start: 4,
-		end: 6
-	    },
-	    "jump": {
-		start: 7,
-		end: 14
-	    }
-	};
-	//instantiate with the id, filename, number of frames, and the animation map.
-	this.hero = new AnimatedSprite("hero", "hero", 21, heroAnimations);
-	this.hero.name = "Ragnar";
+        var heroAnimations = {
+            "stop": {
+                start: 0,
+                end: 0
+            },
+            "run": {
+                start: 1,
+                end: 3
+            },
+            "hit": {
+                start: 4,
+                end: 6
+            },
+            "jump": {
+                start: 7,
+                end: 14
+            }
+        };
+        //instantiate with the id, filename, number of frames, and the animation map.
+        this.hero = new AnimatedSprite("hero", "hero", 21, heroAnimations);
+        this.hero.name = "Ragnar";
 
-	this.hero.animate("stop");
-	this.hero.animationSpeed = 10;
-	this.hero.position.x = this.centerPoint.x;
-	this.hero.position.y = this.centerPoint.y;
-	this.hero.scale = {
-	    x: .8,
-	    y: .5
-	};
+        this.hero.animate("stop");
+        this.hero.animationSpeed = 10;
+        this.hero.position.x = this.centerPoint.x;
+        this.hero.position.y = this.centerPoint.y;
+        this.hero.scale = {
+            x: .8,
+            y: .5
+        };
 
-	this.floor = new DisplayObjectContainer("floor");
-	this.background = new DisplayObjectContainer("background");
-	this.foreground = new DisplayObjectContainer("foreground");
+        this.floor = new DisplayObjectContainer("floor");
+        this.background = new DisplayObjectContainer("background");
+        this.foreground = new DisplayObjectContainer("foreground");
     }
 
     setupMap(root, map) {
 
-	this.floor.position.x = -50;
-	this.floor.position.y = -50;
+        this.floor.position.x = -50;
+        this.floor.position.y = -50;
 
-	for (var y = 0; y < this.tileCount.y; y++) {
-	    var row = [];
-	    for (var x = 0; x < this.tileCount.x; x++) {
-		var bgTile = new Sprite("bg_(" + x + ", " + y + ")", map.background[x][y]);
-		var fgTile = new Sprite(map.foreground[x][y] + "_(" + x + ", " + y + ")", map.foreground[x][y]); 
-		bgTile.position = {
-		    x: x * this.tileSize,
-		    y: y * this.tileSize
-		};
-		fgTile.position = {
-		    x: x * this.tileSize,
-		    y: y * this.tileSize
-		};
-		bgTile.collisionDisable = true;
-		this.background.children.push(bgTile);
-		this.foreground.children.push(fgTile);
-	    }
-	}
-	
-	this.background.collisionDisable = true;
-	this.floor.children.push(this.background);
-	this.floor.children.push(this.foreground);
-	this.root.children = [this.floor, this.hero];
+        for (var y = 0; y < this.tileCount.y; y++) {
+            var row = [];
+            for (var x = 0; x < this.tileCount.x; x++) {
+                var bgTile = new Sprite("bg_(" + x + ", " + y + ")", map.background[x][y]);
+                var fgTile = new Sprite(map.foreground[x][y] + "_(" + x + ", " + y + ")", map.foreground[x][y]);
+                bgTile.position = {
+                    x: x * this.tileSize,
+                    y: y * this.tileSize
+                };
+                fgTile.position = {
+                    x: x * this.tileSize,
+                    y: y * this.tileSize
+                };
+                bgTile.collisionDisable = true;
+                this.background.children.push(bgTile);
+                this.foreground.children.push(fgTile);
+            }
+        }
+
+        this.background.collisionDisable = true;
+        this.floor.children.push(this.background);
+        this.floor.children.push(this.foreground);
+        this.root.children = [this.floor, this.hero];
     }
 
     addActions() {
-	this.actionManager.add("stone-hit", (object) => {
-	    object.visible = false;
-	    object.collisionDisable = true;
-	});
-	this.actionManager.add("bear-fight", (bear) => {
-	    this.combatManager.beginBattle(bear, this.hero, this.gameState.actionsUnlocked, null);
-	    this.inBattle = true;
-	});
+        this.actionManager.add("stone-hit", (object) => {
+            object.visible = false;
+            object.collisionDisable = true;
+        });
+        this.actionManager.add("bear-fight", (bear) => {
+            this.combatManager.beginBattle(bear, this.hero, this.gameState.actionsUnlocked, null);
+            this.inBattle = true;
+        });
     }
 
     concludeBattle(result, opponent) {
-	this.inBattle = false;
-	console.log("[Saga] Conclude battle: " + result);
-	var config = ToastManager.top_right();
-	config.duration = 5000;
-	if (result === "win") {
-	    this.toastManager.put("proximity-context", 
-				  "You defeated the " + opponent.type + "!", 
-				  ["Gained 3 reputation"],
-				  config);
-	}
-	else {
-	    // TODO handle loss
-	}
-	// TODO delete sprite
-	opponent.visible = false;
-	opponent.collisionDisable = true;
+        this.inBattle = false;
+        console.log("[Saga] Conclude battle: " + result);
+        var config = ToastManager.top_right();
+        config.duration = 5000;
+        if (result === "win") {
+            this.toastManager.put("proximity-context",
+                "You defeated the " + opponent.type + "!", ["Gained 3 reputation"],
+                config);
+        } else {
+            // TODO handle loss
+        }
+        // TODO delete sprite
+        opponent.visible = false;
+        opponent.collisionDisable = true;
     }
 
     collideCheck(node, offset) {
-	for (var child of node.children) {
-	    if (child.id === "hero")
-		return;
-	    if (!child.collisionDisable) {
-		var dir = this.hero.collidesWith(child, offset);
-		if (dir) {
-		    var event;
-		    if (dir === "proximity") {
-			event = new Event("proximity-collision", this.dispatcher, {
-			    first: this.hero,
-			    second: child,
-			    direction: dir,
-			    offset: offset
-			});
-		    } else {
-			event = new Event("collision", this.dispatcher, {
-			    first: this.hero,
-			    second: child,
-			    direction: dir,
-			    offset: offset
-			});
-		    }
-		    this.dispatcher.dispatchEvent(event);
-		}
+        for (var child of node.children) {
+            if (child.id === "hero")
+                return;
+            if (!child.collisionDisable) {
+                var dir = this.hero.collidesWith(child, offset);
+                if (dir) {
+                    var event;
+                    if (dir === "proximity") {
+                        event = new Event("proximity-collision", this.dispatcher, {
+                            first: this.hero,
+                            second: child,
+                            direction: dir,
+                            offset: offset
+                        });
+                    } else {
+                        event = new Event("collision", this.dispatcher, {
+                            first: this.hero,
+                            second: child,
+                            floor: this.floor,
+                            direction: dir,
+                            offset: offset
+                        });
+                        switch (dir) {
+                            case "left":
+                            if (this.isCentered("left")) {
+                                    this.floor.position.x += 1.5;
+                                } else {
+                                    this.hero.position.x -= 1.5;
+                                }
+                                break;
+                            case "right":
+                            if (this.isCentered("right")) {
+                                    this.floor.position.x -= 1.5;
+                                } else {
+                                    this.hero.position.x += 1.5;
+                                }
+                                break;
+                            case "top":
+                            if (this.isCentered("up")) {
+                                    this.floor.position.y -= 1.5;
+                                } else {
+                                    this.hero.position.y += 1.5;
+                                }
+                                break;
+                            case "bottom":
+                            if (this.isCentered("down")) {
+                                    this.floor.position.y += 1.5;
 
-	    }
-	    if (child.children) {
-		this.collideCheck(child, {
-		    x: offset.x + node.position.x,
-		    y: offset.y + node.position.y
-		});
-	    }
-	}
+                                } else {
+                                    this.hero.position.y -= 1.5;
+                                }
+                                break;
+                        }
+                    }
+                    this.dispatcher.dispatchEvent(event);
+                }
+
+            }
+            if (child.children) {
+                this.collideCheck(child, {
+                    x: offset.x + node.position.x,
+                    y: offset.y + node.position.y
+                });
+            }
+        }
     }
 
     physic(root) {
-	for (var child of root.children) {
-	    if (!!child.children) {
-		this.physic(child);
-	    }
-	}
+        for (var child of root.children) {
+            if (!!child.children) {
+                this.physic(child);
+            }
+        }
+    }
+
+    isCentered(dir) {
+        var movementSpeed = 1.5;
+
+        if (dir === "right")
+            return this.mapSize.x + this.floor.position.x > this.size.x && Math.abs(this.hero.position.x - this.centerPoint.x) < 8 * movementSpeed;
+        if (dir === "left")
+            return this.floor.position.x < -movementSpeed * 2 && Math.abs(this.hero.position.x - this.centerPoint.x) < 8 * movementSpeed;
+        if (dir === "up")
+            return this.mapSize.y + this.floor.position.y > this.size.y && Math.abs(this.hero.position.y - this.centerPoint.y) < 8 * movementSpeed;
+        if (dir === "down")
+            return this.floor.position.y < -movementSpeed && Math.abs(this.hero.position.y - this.centerPoint.y) < 8 * movementSpeed;
     }
 
     move(pressedKeys, newKeys) {
-	var movementSpeed = 1.5;
-	if (pressedKeys.includes(keycodes.right)) {
-	    this.hero.animate("run");
+        var movementSpeed = 1.5;
+        if (pressedKeys.includes(keycodes.right)) {
+            this.hero.animate("run");
 
-	    //move
-	    if (this.mapSize.x + this.floor.position.x > this.size.x && Math.abs(this.hero.position.x - this.centerPoint.x) < 2 * movementSpeed)
-		this.floor.position.x -= movementSpeed;
-	    else if (this.hero.position.x < this.size.x)
-		this.hero.position.x += movementSpeed;
-	}
-	if (pressedKeys.includes(keycodes.left)) {
-	    this.hero.animate("run");
+            //move
+            if (this.isCentered("right"))
+                this.floor.position.x -= movementSpeed;
+            else if (this.hero.position.x < this.size.x)
+                this.hero.position.x += movementSpeed;
+        }
+        if (pressedKeys.includes(keycodes.left)) {
+            this.hero.animate("run");
 
-	    //move
-	    if (this.floor.position.x < -1.5 && Math.abs(this.hero.position.x - this.centerPoint.x) < 2 * movementSpeed)
-		this.floor.position.x += movementSpeed;
-	    else if (this.hero.position.x > 0) {
-		this.hero.position.x -= movementSpeed;
-	    }
-	}
-	if (pressedKeys.includes(keycodes.down)) {
-	    this.hero.animate("run");
-	    if (this.mapSize.y + this.floor.position.y > this.size.y && Math.abs(this.hero.position.y - this.centerPoint.y) < 2 * movementSpeed)
-		this.floor.position.y -= movementSpeed;
-	    else if (this.hero.position.y < this.size.y)
-		this.hero.position.y += movementSpeed;
-	}
-	if (pressedKeys.includes(keycodes.up)) {
-	    this.hero.animate("run");
-	    if (this.floor.position.y < -1.5 && Math.abs(this.hero.position.y - this.centerPoint.y) < 2 * movementSpeed)
-		this.floor.position.y += movementSpeed;
-	    else if (this.hero.position.y > 0) {
-		this.hero.position.y -= movementSpeed;
-	    }
-	}
+            //move
+            if (this.isCentered("left"))
+                this.floor.position.x += movementSpeed;
+            else if (this.hero.position.x > 0) {
+                this.hero.position.x -= movementSpeed;
+            }
+        }
+        if (pressedKeys.includes(keycodes.down)) {
+            this.hero.animate("run");
+            if (this.isCentered("up"))
+                this.floor.position.y -= movementSpeed;
+            else if (this.hero.position.y < this.size.y)
+                this.hero.position.y += movementSpeed;
+        }
+        if (pressedKeys.includes(keycodes.up)) {
+            this.hero.animate("run");
+            if (this.isCentered("down"))
+                this.floor.position.y += movementSpeed;
+            else if (this.hero.position.y > 0) {
+                this.hero.position.y -= movementSpeed;
+            }
+        }
 
-	if (newKeys.includes(keycodes.i)) {
+        if (newKeys.includes(keycodes.i)) {
             this.inventoryManager.open(dev_inventory);
         }
 
@@ -270,7 +338,7 @@ class Saga extends Game {
     }
 
     pressedKeyDiff(pressedKeys) {
-        if(!this.lastPressed)
+        if (!this.lastPressed)
             this.lastPressed = [];
         var t = this;
         var newKeys = pressedKeys.filter(function(k1) {
@@ -293,7 +361,7 @@ class Saga extends Game {
         });
         if (this.inBattle) {
             this.combatManager.update(newKeys);
-        } else if(this.inInventory) {
+        } else if (this.inInventory) {
             this.inventoryManager.update(newKeys);
         } else {
             this.move(pressedKeys, newKeys);
--- engine/quests/QuestManager.js
+++ engine/quests/QuestManager.js
@@ -11,33 +11,24 @@ class QuestManager extends EventListener {
             if (event.params.first.id === "hero" && event.params.second.id === "coin") {
                 var pickedUpEvent = new Event("coin-picked-up", event.source);
                 event.source.dispatchEvent(pickedUpEvent);
-            } else {
-                var hero = event.params.first;
-                var wall = event.params.second;
-                var offset = event.params.offset;
-                if (event.params.direction === "left") {
-                    hero.position.x = offset.x + wall.position.x - hero.width * hero.scale.x;
-                } else if (event.params.direction === "right") {
-                    hero.position.x = offset.x + wall.position.x + wall.width * wall.scale.x + 1.5;
-                } else if (event.params.direction === "top") {
-                    hero.position.y = offset.y + wall.position.y + wall.height * wall.scale.y + 1.5;
-                } else if (event.params.direction === "bottom") {
-                    hero.position.y = offset.y + wall.position.y - hero.height * hero.scale.y;
-                }
             }
         }
-	// TODO here
-	if (event.eventType === "proximity-collision") {
-	    let params = event.params;
-	    if (params.second.type === "rock") {
-		this.toasts.put("proximity-context", "...", ["You stare at the rock.", "The rock stares back.", "<SPC>"], {duration: 300});
-		var dist = params.first.distanceFrom(params.second);
-		this.actions.focus(params.second, dist, "stone-hit");
-	    } else if (params.second.type === "bear") {
-		this.toasts.put("proximity-context", "...", ["Yeah. Big ass-bear.", "Fight him?", "<SPC>"], {duration: 300});
-		var dist = params.first.distanceFrom(params.second);
-		this.actions.focus(params.second, dist, "bear-fight");
-	    }
-	}
+        // TODO here
+        if (event.eventType === "proximity-collision") {
+            let params = event.params;
+            if (params.second.type === "rock") {
+                this.toasts.put("proximity-context", "...", ["You stare at the rock.", "The rock stares back.", "<SPC>"], {
+                    duration: 300
+                });
+                var dist = params.first.distanceFrom(params.second);
+                this.actions.focus(params.second, dist, "stone-hit");
+            } else if (params.second.type === "bear") {
+                this.toasts.put("proximity-context", "...", ["Yeah. Big ass-bear.", "Fight him?", "<SPC>"], {
+                    duration: 300
+                });
+                var dist = params.first.distanceFrom(params.second);
+                this.actions.focus(params.second, dist, "bear-fight");
+            }
+        }
     }
 }
